8f0ba8cbf3f7d75cf872130dbbe196710c818461
wcm <wcm@noemail.net>
2020-11-07 00:02:13 +0000

    Add missing include.
    FossilOrigin-Name: c857b626da5e725a148edf3bd9c237000fc38a85452ded00451b15b2aa1d3321

--- unveil.scm
+++ unveil.scm
@@ -1,7 +1,8 @@
 (module (unveil)
   (unveil unveil-lock)
 
-  (import (only (chicken base) include)
+  (import (scheme)
+          (only (chicken base) include)
           (chicken foreign))
 
   (include "unveil-impl.scm"))
