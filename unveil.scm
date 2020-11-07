29a979bfc30cabd0e57fea2e9b607d1614821818
wcm <wcm@noemail.net>
2020-11-07 00:21:40 +0000

    Use disable-interrupts.
    FossilOrigin-Name: 9708abfc7169371d96f3b55fc480cf61e7b248aa9e0ea44e63ed5b55f85b536d

--- unveil.scm
+++ unveil.scm
@@ -2,7 +2,8 @@
   (unveil unveil-lock)
 
   (import (scheme)
-          (only (chicken base) include)
+          (only (chicken base) declare include)
           (chicken foreign))
 
+  (declare (disable-interrupts))
   (include "unveil-impl.scm"))
