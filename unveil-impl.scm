42a2d8473bc367071d578efa571597402061f008
wcm <wcm@noemail.net>
2020-11-22 18:19:07 +0000

    Pass errno.
    FossilOrigin-Name: 1afe2e6bbb0bfab61a59f72eaaf893b3fa3e4ddef6facb0685d62ede291acdb0

--- unveil-impl.scm
+++ unveil-impl.scm
@@ -8,18 +8,17 @@
 
 (define (unveil path permissions)
   (unless (zero? (c-unveil path permissions))
-    (handle-unveil-error)))
+    (handle-unveil-error (errno))))
 
 (define (unveil-lock)
   (unless (zero? (c-unveil #f #f))
-    (handle-unveil-error)))
+    (handle-unveil-error (errno))))
 
-(define (handle-unveil-error)
+(define (handle-unveil-error err)
   (let ((desc `((,errno/2big .  "per-process unveil limit exceeded")
                 (,errno/noent . "unreadable path")
                 (,errno/inval . "invalid permission string")
-                (,errno/perm  . "access violation or unveil already locked")))
-        (err (errno)))
+                (,errno/perm  . "access violation or unveil already locked"))))
     (error (cond ((assv err desc) => cdr)
                  (else "(unknown error)"))
            err)))
