bc7ae5e9a07742bf7b74e4cbe4ecc7d91ff924c5
wcm <wcm@noemail.net>
2020-11-22 17:57:41 +0000

    errno checking and error handling.
    FossilOrigin-Name: d951158a9ad8c6ef3854fa6bb1b2ab8c20a28bd3908fc5f2c625cf11f923f670

--- unveil-impl.scm
+++ unveil-impl.scm
@@ -1,9 +1,23 @@
 (foreign-declare "#include <unistd.h>")
+(foreign-declare "#include <errno.h")
+
+(define-foreign-variable errno/2big int "E2BIG")
 
 (define c-unveil
   (foreign-lambda int unveil (const c-string) (const c-string)))
 
 (define (unveil path permissions)
-  (c-unveil path permissions))
+  (unless (zero? (c-unveil path permissions))
+    (handle-unveil-error)))
 
-(define (unveil-lock) (c-unveil #f #f))
+(define (unveil-lock)
+  (unless (zero? (c-unveil #f #f))
+    (handle-unveil-error)))
+
+(define (handle-unveil-error)
+  (error (case (errno)
+           ((errno/2big)  "per-process unveil limit exceeded")
+           ((errno/noent) "unreadable path")
+           ((errno/inval) "invalid permission string")
+           ((errno/eperm) "access violation or unveil() already locked")
+           (else          "(unknown error)"))))
