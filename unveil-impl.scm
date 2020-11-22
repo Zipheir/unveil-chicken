bc745b369eab581f5c89aeb9e521cf7b78cb8caa
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-11-22 15:48:39 -0500

    Change errno access approach.

--- unveil-impl.scm
+++ unveil-impl.scm
@@ -3,22 +3,24 @@
 
 (define-foreign-variable errno/2big int "E2BIG")
 
+(define-foreign-variable _errno int "errno")
+
 (define c-unveil
   (foreign-lambda int unveil (const c-string) (const c-string)))
 
 (define (unveil path permissions)
   (unless (zero? (c-unveil path permissions))
-    (handle-unveil-error (errno))))
+    (handle-unveil-error)))
 
 (define (unveil-lock)
   (unless (zero? (c-unveil #f #f))
-    (handle-unveil-error (errno))))
+    (handle-unveil-error)))
 
-(define (handle-unveil-error err)
+(define (handle-unveil-error)
   (let ((desc `((,errno/2big .  "per-process unveil limit exceeded")
                 (,errno/noent . "unreadable path")
                 (,errno/inval . "invalid permission string")
                 (,errno/perm  . "access violation or unveil already locked"))))
-    (error (cond ((assv err desc) => cdr)
+    (error (cond ((assv _errno desc) => cdr)
                  (else "(unknown error)"))
-           err)))
+           _errno)))
